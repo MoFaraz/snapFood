0c86cc2d2ce1397107f3069140495bd64e5b43da
mofaraz <mopharaz@gmail.com>
2022-06-04 19:51:56 +0430

    Create Login Part

---
+++ src/main/java/fourthproject/snapfood/Controller/LoginController.java
@@ -0,0 +1,54 @@
+package fourthproject.snapfood.Controller;
+
+import fourthproject.snapfood.Main;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController{
+
+    @FXML
+    private CheckBox checkBox;
+
+    @FXML
+    private Label loginLBL;
+
+    @FXML
+    private Label passwordLBL;
+
+    @FXML
+    private PasswordField passwordTXT;
+
+    @FXML
+    private Button signInBTN;
+
+    @FXML
+    private Label userLBL;
+
+    @FXML
+    private TextField userTXT;
+
+    @FXML
+    void forgetPassAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newUserAction(ActionEvent event) {
+
+    }
+
+}
