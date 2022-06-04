a29a1ed310a820efd45f662e8a161b6d2ef65392
mofaraz <mopharaz@gmail.com>
2022-06-04 19:50:56 +0430

    Create SignUp Part

---
+++ src/main/java/fourthproject/snapfood/Controller/SignUpController.java
@@ -0,0 +1,55 @@
+package fourthproject.snapfood.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private Button backBTN;
+
+    @FXML
+    private PasswordField conPassFLD;
+
+    @FXML
+    private TextField emailFLD;
+
+    @FXML
+    private Label emailLBL;
+
+    @FXML
+    private Label passwordLBL;
+
+    @FXML
+    private Label passwordLBL1;
+
+    @FXML
+    private PasswordField passwordTXT;
+
+    @FXML
+    private Button signUpBTN;
+
+    @FXML
+    private Label signUpLBL;
+
+    @FXML
+    private Label userLBL;
+
+    @FXML
+    private TextField userTXT;
+
+    @FXML
+    void backBTNAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void signUpAction(ActionEvent event) {
+
+    }
+
+}
